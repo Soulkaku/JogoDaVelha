e2ce4411c647229741f102f4d7ae23e65d16b3fd
Thiago Gonçalves <tangogitlab@gmail.com>
2025-03-27 16:05:20 -0300

    server criado

--- server.js
+++ server.js
@@ -6,16 +6,17 @@ import path from "path";
 import { fileURLToPath } from "url"
 
 const app = express();
-const httpServer = http.createServer(app); //servidor http criado e receber as funções do express.
+const httpServer = http.createServer(app); //servidor http criado e receber as funcionalidades do express.
 const io = new Server(httpServer);
 
-io.on("connection", (socket) => {
-    console.log("User connected:", socket.id);
-});
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.use(express.static(path.join(__dirname, "public")));
+
 httpServer.listen(3000, () => {
     console.log("Listen! from localHost:3000");
 })
-
 
 
 export default io;
