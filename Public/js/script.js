69beca3cd62a087f08fb4eaa2cf10c862a974e4b
Soulkaku <tangogitlab@gmail.com>
2025-04-01 21:38:02 -0300

    preservado

--- Public/js/script.js
+++ Public/js/script.js
@@ -1,27 +1,56 @@
-    const socket = io.connect();
-    
-    const inputText = document.getElementById("userForm");
+// const socket = io.connect();
 
-    inputText.addEventListener("submit", function (event) {
-        event.preventDefault();
-    
-        var createUser = document.getElementById("userInput");
-        var username = createUser.value.trim();
-        
-        if (!username) {
-            alert("username empty");
-            return;
-        }
+// let user; //object
 
-        function createSession(id, name) {
-            // socket.emit("create", "room");
-        };
-        // const userId = socket.id;
-        // const userData = {
-        //     name: username,
-        //     id: socket.id
-        // }
+// const inputText = document.getElementById("UserForm");
+
+// inputText.addEventListener("submit", function(event) {
+//     event.preventDefault();
+
+//     var createUser = document.getElementById("userInput");
+    
+//     var username = createUser.value.trim();
+//     var id = socket.id;
+// });
+//     const socket = io.connect();
+    
+//     const inputText = document.getElementById("userForm");
+    
+//     let user = {
+//         name: username,
+//         userId: id
+//     };
+    
+//     inputText.addEventListener("submit", function (event) {
+//         event.preventDefault();
+    
+//         var createUser = document.getElementById("userInput");
+
+//         var username = createUser.value.trim();
+//         var id = socket.id;
+
+//         user = {
+//             name: username,
+//             userId: id 
+//         }
+
+//         if (!username) {
+//             alert("username empty");
+//             return;
+//         }
+
+//         function createSession(id, name) {
+//             // socket.emit("create", "room");
+//         };
+//         // const userId = socket.id;
+//         // const userData = {
+//         //     name: username,
+//         //     id: socket.id
+//         // }
         
-        // return userData;
-        
-    });
+//         // return userData;
+//        console.log(user);
+
+//     });
+
+// export { user }   
