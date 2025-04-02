3a0d0b9aabd7b796273434f32affcfcbcfe2bc00
Soulkaku <tangogitlab@gmail.com>
2025-04-02 17:45:32 -0300

    blocos localizados pelo data-id e conteudo alterado atraves do click

--- Public/js/gameScript.js
+++ Public/js/gameScript.js
@@ -1,9 +1,34 @@
 const socket = io();
 
-const buttons = document.getElementsByClassName("box");
+var boxes = document.getElementsByClassName("box");
 
-buttons.addEventListener("click", function(event) {
-    event.preventDefault();
+const winPatterns = []
 
-    console.log("!");
-});
+
+// function findposition() {
+//     const position = this.getAttribute("data-id");
+
+//     // alert(position);
+
+//     return position;
+// }
+
+function markBox() {
+    const position = this.getAttribute("data-id");
+
+    let box = document.querySelector(`[data-id="${position}"]`);
+    box.innerHTML = "X";
+}
+
+//if 1, 2, 3 || 1, 5, 9 || 4, 5, 6 || ... === same value, return Victory();
+
+// boxes.forEach(box => {
+//     box.addEventListener("click", function () {
+//         const dataId = this.getAttribute("data-id");
+//         alert(dataId);
+//     })
+// })
+
+for (let i = 0; i < boxes.length; i++) {
+    boxes[i].addEventListener("click", markBox , false);
+}
