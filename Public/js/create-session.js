460401777acbe04de49d5ed6a9f5638489fd17e5
Soulkaku <tangogitlab@gmail.com>
2025-04-02 17:48:17 -0300

    nome agora enviado pelo websocket

--- Public/js/create-session.js
+++ Public/js/create-session.js
@@ -2,23 +2,34 @@ const socket = io.connect()
 
 const inputText = document.getElementById("userForm");
 
-let username;
-let userId;
 
+const userData = new Object();
 
 inputText.addEventListener("submit", (event) => {
     event.preventDefault();
 
+    //let userId = socket.id;
+    
     const nameInput = document.getElementById("userInput");
+    userData.name = nameInput.value.trim();
+
+    createSession(userData);
     
-    username = nameInput.value.trim();
-    userId = socket.id;
-
-
-    if(!username) {
+    if(!createSession) {
         alert("type a name, no matter what");
-    }
+        return;
 
-    console.log(username);
-    
+    } else {
+        nameInput.value = "";
+        socket.emit("userTest", alert(JSON.stringify(userData))); //returns object object
+        // // socket.emit('create', "room1"); //create a room;
+        
+         window.location.href = "http://localhost:3000/game"
+    }
 });
+
+function createSession(userData) {
+    socket.emit("dados-do-usuario", userData);
+};
+
+
