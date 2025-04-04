8ff5565d8e61ef654601cba9ddc44d23d60a960c
Soulkaku <tangogitlab@gmail.com>
2025-04-04 17:41:09 -0300

    logica de jogo sendo feita

--- Public/js/gameScript.js
+++ Public/js/gameScript.js
@@ -6,54 +6,49 @@ const yourName = document.getElementById("user1");
 const enemyName = document.getElementById("user2");
 
 const params = new URLSearchParams(window.location.search);
-const room = params.get("room");
-console.log(`Entrando na sala: ${room}, Usuário: ${username}`);
 
-const username = sessionStorage.getItem("username");
+const room = params.get("room");
+const username = params.get("username");
 const userId = sessionStorage.getItem("identification");
 
+console.log(`Entrando na sala: ${room}, Usuário: ${username}`);
+
+
+
 yourName.textContent = `X-player: ${username}`;
-
-
-// enemyName.textContent = `O-player: ${ }`
-
-
-// const board = ["", "", "", "", "", "", "", "", ""]; // Representação do tabuleiro
-// let currentPlayer = "X"; // Começa com "X"
-
-// const winPatterns = [
-//     [0, 1, 2], [3, 4, 5], [6, 7, 8], // Linhas
-//     [0, 3, 6], [1, 4, 7], [2, 5, 8], // Colunas
-//     [0, 4, 8], [2, 4, 6] // Diagonais
-// ];
-
-// function checkWin() {
-//     for (let pattern of winPatterns) {
-//         const [a, b, c] = pattern;
-//         if (board[a] && board[a] === board[b] && board[a] === board[c]) {
-//             alert(`${board[a]} venceu!`);
-//             return true;
-//         }
-//     }
-//     return false;
-// }
-
-function markBox() {
-    const position = this.getAttribute("data-id");
-    
-    if (board[position] !== "") return; // Se já tiver um valor, não faz nada
-
-    board[position] = currentPlayer;
-    this.innerHTML = currentPlayer;
-
-    if (checkWin()) {
-        setTimeout(() => location.reload(), 2000); // Reinicia o jogo após 2s
-        return;
-    }
-
-    currentPlayer = currentPlayer === "X" ? "O" : "X"; // Alterna jogador
-}
+// enemyName.textContent = `O-player: ${enemyName}`;
 
 for (let i = 0; i < boxes.length; i++) {
-    boxes[i].addEventListener("click", markBox);
+    boxes[i].addEventListener("click", function() {
+        const position = this.getAttribute("data-id");
+        
+        let board = [   "1", "2", "3", 
+                        "4", "5", "6",
+                        "7", "8", "9",
+                    ]
+        console.log(board[i]);
+        if (board [1] == board [2] == board [3]) {
+            console.log("winner");
+        }
+    });
 }
+
+// function markBox() {
+//     const position = this.getAttribute("data-id");
+    
+//     if (board[position] !== "") return; // Se já tiver um valor, não faz nada
+
+//     board[position] = currentPlayer;
+//     this.innerHTML = currentPlayer;
+
+//     if (checkWin()) {
+//         setTimeout(() => location.reload(), 2000); // Reinicia o jogo após 2s
+//         return;
+//     }
+
+//     currentPlayer = currentPlayer === "X" ? "O" : "X"; // Alterna jogador
+// }
+
+// for (let i = 0; i < boxes.length; i++) {
+//     boxes[i].addEventListener("click", markBox);
+// }
