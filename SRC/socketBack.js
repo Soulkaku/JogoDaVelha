9b3046c3e16bc42b6ca17825493331ada55fdf35
Soulkaku <tangogitlab@gmail.com>
2025-04-07 17:15:59 -0300

    reiniciado

--- SRC/socketBack.js
+++ SRC/socketBack.js
@@ -3,5 +3,9 @@ import io from "../server.js";
 io.on("connection", (socket) => {
     console.log(`Client ${socket.id} connected`);
 
+    socket.on("create-session", sessionData => {
+        const room = sessionData.room;
 
+        console.log(sessionData);
+    })
 });
