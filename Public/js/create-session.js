51e4466ed9d5be2ce1933468b3a5a53ff9ff2e32
Soulkaku <tangogitlab@gmail.com>
2025-04-07 16:47:24 -0300

    agora é um objeti a room e username

--- Public/js/create-session.js
+++ Public/js/create-session.js
@@ -1,29 +1,13 @@
 const socket = io();
-//create-session(name, room)
-
+//create-session( {name, room}
 
 function createSession(userInfo ,room) {
-    socket.emit("create-session", userInfo, room);
-    // socket.join(room);
+    const sessionData = {
+        name: userInfo,
+        room: room
+    };
 
-    // const roomData = io.sockets.adapter.rooms.get(room);
-    // const usersInRoom = roomData ? roomData.size : 0; //evitar um undefined
-
-    // if(usersInRoom === 0) {
-    //     console.log("no players");
-    //     // socket.emit("enemy", userInfo);
-    //     socket.on("enter-room", userInfo ,room);
-    // } else {
-    //     socket.on("create-room", userInfo, room);
-    // }
+    socket.emit("create-session", sessionData);
 };
-
-// function enterSession(userInfo, room) {
-//     socket.on("enter-session", userInfo, room);
-// }
-
-    // socket.on("enter-session", (userInfo, room) => {
-
-    // });
 
 export { createSession };
