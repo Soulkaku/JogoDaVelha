476489ab2b16df1a9940154226e46e06ae7d1378
Soulkaku <tangogitlab@gmail.com>
2025-04-07 16:52:28 -0300

    remoção do socket na criação de usuario

--- Public/js/userScript.js
+++ Public/js/userScript.js
@@ -1,87 +1,27 @@
-const socket = io.connect();
-
+// import { createSession } from "./create-session.js";
 
 const form = document.getElementById("userForm");
-// const userData = new Object();
 
 form.addEventListener("submit", event => {
     event.preventDefault();
 
     const room = (document.getElementById("roomInput")).value;
     const username = (document.getElementById("userInput")).value.trim();
-  
-    // userData.name = username.value.trim();
     
     if(!username) {
         alert("escreva um nome");
         return;
 
     } else {
-            sessionStorage.setItem("username", username);
-            sessionStorage.setItem("indentification", socket.id);
+            // sessionStorage.setItem("username", username);
+            // sessionStorage.setItem("room", room);
 
-            socket.emit("create_user-find_room", {name : username, id: socket.id}, room);
+            // createSession(username, room);
 
-            document.getElementById("userInput").value = "";
-            document.getElementById("roomInput").value = "";
+            // document.getElementById("userInput").value = "";
+            // document.getElementById("roomInput").value = "";
+
+            window.location.href = `/game?room=${room}&username=${username}`;
     }
 });
 
-
-// const roomForm = document.getElementById("roomForm");
-// let room;
-
-// roomForm.addEventListener("submit", (event) => {
-//     event.preventDefault
-//     const roomId = document.getElementById("roomInput");
-    
-
-//     if(roomId.value === "") {
-//         socket.emit()
-//     }
-//     socket.emit("enter-room", roomId);
-// });
-// const inputText = document.getElementById("userForm");
-// const userData = new Object();
-
-// inputText.addEventListener("submit", (event) => {
-//     event.preventDefault();
-    
-//     let nameInput = document.getElementById("userInput");
-//     userData.name = nameInput.value.trim();
-
-//     if (!userData.name) {
-//         alert("digite um nome");
-//         return;
-//     } else {
-//         alert(JSON.stringify(userData));
-//         sessionStorage.setItem("username", userData.name);
-//         socket.emit("create-user", { name : userData.name });
-        
-
-//         // nameInput.value = "";
-//         window.location.href = "http://localhost:3000/game"
-//     }
-
-    // socket.on("alert-user", (userName) => { //somente para testar conexão
-    //     console.log(userName);
-    // });
-    
-    // createSession(userData);
-    
-    // if(!createSession) {
-    //     alert("type a name, no matter what");
-    //     return;
-
-    // } else {
-    //     nameInput.value = "";
-    //     socket.emit("userTest", alert(JSON.stringify(userData))); //returns object object
-    //     socket.emit("user-created", {name : userData.name});
-        
-    //      window.location.href = "http://localhost:3000/game"
-    // }
-// });
-
-// function createSession(userData) {
-//     socket.emit("dados-do-usuario", userData);
-// };
