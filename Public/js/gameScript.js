8139170d7049c472e23c8b4bf8b0b750b5b45649
Soulkaku <tangogitlab@gmail.com>
2025-04-07 17:14:26 -0300

    aplicação do nome no h2

--- Public/js/gameScript.js
+++ Public/js/gameScript.js
@@ -1,54 +1,19 @@
+import { createSession } from "./create-session.js";
 const socket = io();
-
-const boxes = document.getElementsByClassName("box");
-
-const yourName = document.getElementById("user1");
-const enemyName = document.getElementById("user2");
 
 const params = new URLSearchParams(window.location.search);
 
+const name = params.get("username");
 const room = params.get("room");
-const username = params.get("username");
-const userId = sessionStorage.getItem("identification");
-
-console.log(`Entrando na sala: ${room}, Usuário: ${username}`);
 
 
+    createSession(name, room);
 
-yourName.textContent = `X-player: ${username}`;
-// enemyName.textContent = `O-player: ${enemyName}`;
 
-for (let i = 0; i < boxes.length; i++) {
-    boxes[i].addEventListener("click", function() {
-        const position = this.getAttribute("data-id");
-        
-        let board = [   "1", "2", "3", 
-                        "4", "5", "6",
-                        "7", "8", "9",
-                    ]
-        console.log(board[i]);
-        if (board [1] == board [2] == board [3]) {
-            console.log("winner");
-        }
-    });
-}
+const yourName = document.getElementById("Your-User");
+yourName.textContent = `Ally: ${yourName}`;
 
-// function markBox() {
-//     const position = this.getAttribute("data-id");
-    
-//     if (board[position] !== "") return; // Se já tiver um valor, não faz nada
-
-//     board[position] = currentPlayer;
-//     this.innerHTML = currentPlayer;
-
-//     if (checkWin()) {
-//         setTimeout(() => location.reload(), 2000); // Reinicia o jogo após 2s
-//         return;
-//     }
-
-//     currentPlayer = currentPlayer === "X" ? "O" : "X"; // Alterna jogador
-// }
-
-// for (let i = 0; i < boxes.length; i++) {
-//     boxes[i].addEventListener("click", markBox);
-// }
+const enemyName = document.getElementById("Enemy-User");
+socket.on("enemy-data", enemyName => {
+    enemyName.textContent = `enemy: ${enemyName}`;
+});
