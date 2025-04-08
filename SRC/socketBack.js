b1bf9e5a1d31b14cb879ab2ca3d2bb0827c76988
Soulkaku <tangogitlab@gmail.com>
2025-04-08 15:40:26 -0300

    conexão feita com o server para enviar para o jogo a posicao

--- SRC/socketBack.js
+++ SRC/socketBack.js
@@ -7,5 +7,16 @@ io.on("connection", (socket) => {
         const room = sessionData.room;
 
         console.log(sessionData);
-    })
+    });
+    
+    socket.on("join-room", (room) => {
+        socket.join(room);
+        console.log(`${socket.id} entered in room ${room}`);
+    });
+    
+        socket.on("position", (session) => {
+            const room = session.room;
+            const position = session.position;
+            socket.broadcast.to(room).emit("positionClient", position);
+        });
 });
