5051478b7adc64d0d18346163b4d0b429ba970f9
Soulkaku <tangogitlab@gmail.com>
2025-04-09 16:05:40 -0300

    o usuario escolhe seu simbolo

--- Public/js/userScript.js
+++ Public/js/userScript.js
@@ -1,10 +1,25 @@
 // import { createSession } from "./create-session.js";
 
+const button_chooseSymbol = document.getElementById("buttonChooseSymbol");
+
+let index = 0;
+const symbols = ["X", "O"];
+
+button_chooseSymbol.addEventListener("click", e => {
+    e.preventDefault();
+
+    index = (index + 1) % symbols.length;
+    button_chooseSymbol.textContent = symbols[index];
+
+    sessionStorage.setItem("yourSymbol", symbols[index]);
+});
+
+
 const form = document.getElementById("userForm");
 
 form.addEventListener("submit", event => {
     event.preventDefault();
-
+    
     const room = (document.getElementById("roomInput")).value;
     const username = (document.getElementById("userInput")).value.trim();
     
