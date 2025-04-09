bd87467ff076c50f81ce920a21b6fda430632cbf
Soulkaku <tangogitlab@gmail.com>
2025-04-09 17:06:37 -0300

    socket do jogo para enviar a posição

--- Public/js/play-action.js
+++ Public/js/play-action.js
@@ -1,20 +1,19 @@
-import { Socket } from "socket.js";
+import { socket } from "../socket.js";
 
-const board = [ 0, 1, 2,
-                3, 4, 5,
-                6, 7, 8
-];
+// const board = [ 0, 1, 2,
+//                 3, 4, 5,
+//                 6, 7, 8
+// ];
 
-const winPatterns = [board[0, 1, 2], board[3, 4, 5], board[6, 7, 8], board[0, 3, 6], 
-                     board[1, 4, 7], board[2, 5, 8], 
-                     board[0, 4, 8], board[2, 4, 6]];
+
+
 
 function playAction(username, play) {
 
     const userPlay = {
         position : play,
-        user : username
-    }
+        name : username
+    };
     
     socket.emit("player-action", (userPlay));
 }
