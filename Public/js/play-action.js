8e4c63c9f5cdd5bb70c5bbdc40dbfcfd174ea4c6
Soulkaku <tangogitlab@gmail.com>
2025-04-09 17:46:08 -0300

    remoção dos comentarios

--- Public/js/play-action.js
+++ Public/js/play-action.js
@@ -1,13 +1,5 @@
 import { socket } from "../socket.js";
 
-// const board = [ 0, 1, 2,
-//                 3, 4, 5,
-//                 6, 7, 8
-// ];
-
-
-
-
 function playAction(username, play) {
 
     const userPlay = {
@@ -17,9 +9,5 @@ function playAction(username, play) {
     
     socket.emit("player-action", (userPlay));
 }
-
-// function verifyWin(, play) {
-//     const position = play;
-// }
 
 export { playAction };
