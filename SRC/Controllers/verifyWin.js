6142ca93c9a196998f808347ebf9b53eed69e585
Soulkaku <tangogitlab@gmail.com>
2025-04-11 17:49:32 -0300

    de star wars 'de alguma forma ele voltou' agora no meu codigo 'de alguma forma isso funciono'

--- SRC/Controllers/verifyWin.js
+++ SRC/Controllers/verifyWin.js
@@ -1,18 +1,52 @@
-const winPatterns = [ [1, 2, 3], [4, 5, 6], [7, 8, 9],
-                      [1, 4, 7], [2, 5, 8], [3, 6, 9], 
-                      [1, 3, 9], [3, 5, 7]  ];
+
+const winPatterns = [ 
+        ['1', '2', '3'], ['4', '5', '6'], ['7', '8', '9'],
+        ['1', '4', '7'], ['2', '5', '8'], ['3', '6', '9'], 
+        ['1', '3', '9'], ['3', '5', '7'], 
+        ];
 
 
+// function checkWin(moves) {
+//     return winPatterns.some(combo => combo.every(move => moves.includes(move)));
+// }
 
-function checkWin(moves) {
-    return winPatterns.some(combo => combo.every(move => moves.includes(move)));
+function checkwin(moves) {
+    return winPatterns.some(winCombo => winCombo.every(movement => moves.includes(movement)));
 }
 
-function receivePosition(moves, user) {
+
+function defineWinner(user, moves) {
+    let movement = moves;
     console.log(user, moves);
-    if(checkWin(moves)) {
-        console.log(user + "winner");
+
+    if(checkwin(movement)) {
+        console.log("winner");
+    } else {
     }
+    console.log("not detected");
+    
+        if(movement.length === 3) {
+            console.log(movement + "right");
+        }
+    
 }
 
-export { receivePosition };
+
+// function defineWinner(user, moves) {
+//     let movement = moves;
+//     console.log(user, moves);
+
+//     if(checkwin(movement)) {
+//         console.log(user + "{ winner }");
+//     } else {
+//     }
+//     console.log("lost");
+// }
+export {checkwin, defineWinner };
+// function receivePosition(moves, user) {
+//     console.log(user, moves);
+//     if(checkWin(moves)) {
+//         console.log(user + "winner");
+//     }
+// }
+
